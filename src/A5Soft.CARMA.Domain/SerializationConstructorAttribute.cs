6818024584738945481d77ef43417ffc6823da59
Apskaita5 <programa.apskaita@gmail.com>
2025-06-09 15:49:14 +0300

    added serialization constructor

---
+++ src/A5Soft.CARMA.Domain/SerializationConstructorAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A5Soft.CARMA.Domain
+{
+    /// <summary>
+    /// An attribute to mark a serialization constructor.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+    public class SerializationConstructorAttribute : Attribute
+    {
+    }
+}
